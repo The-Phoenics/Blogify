9bc8307490e32c11fe40d7efdf22e27f8c2f3bc3
Karan <kar9205an@gmail.com>
2025-02-15 14:10:24 +0530

    refactor database connection and interaction

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,7 +1,42 @@
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const database_url = process.env.DATABASE_URL
 
+class Database {
+    public static Get(): Connection | undefined {
+        if (!this.db) {
+            console.log(`INFO: First you need to connect to the database. 
+      Connect by calling: \"Database.Connect(callback)\"`)
+        }
+        return this.db
+    }
+
+    public static async Connect(callback) {
+        if (this.db) {
+            console.log("INFO: Database is already connected")
+            return
+        }
+
+        if (this.db) {
+            console.log("INFO: Database is already connected")
+            return
+        }
+
+        try {
+            const mongo = await mongoose.connect(database_url)
+            this.db = mongo.connection;
+            console.log('SUCCESS: Mongodb connected successfully')
+            callback()
+        } catch (error) {
+            console.log('ERROR: Failed to connect to database: ', error)
+        }
+    }
+
+    private static db: Connection = null
+}
 
 
-export default mongoose;
+export default Database;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import { Request, Response } from "express";
 import app from "./app.js";
-import mongoose from "mongoose";
+import Database from "./db/db.js";
 
 // routes
 import userRouter from "./routes/user.route.js";
@@ -18,14 +18,8 @@ app.get('*', (req: Request, res: Response) => {
     res.status(404).send("404 NOT FOUND")
 })
 
-// connect database
-async function connect() {
-    try {
-        const res = await mongoose.connect(process.env.DATABASE_URL)
-        console.log('SUCCESS: Mongodb connected successfully')
-        app.listen(process.env.PORT, () => console.log(`SUCCESS: Server started at port ${process.env.PORT}`))
-    } catch (error) {
-        console.log('ERROR: Failed to connect to database: ', error)
-    }
+
+const startServer = () => {
+    app.listen(process.env.PORT, () => console.log(`SUCCESS: Server started at port ${process.env.PORT}`))
 }
-connect()
+Database.Connect(startServer)
