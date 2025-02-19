b4bd04e53c405818daa3e7f439906d61d7f76f90
Karan <kar9205an@gmail.com>
2025-02-19 12:50:23 +0530

    finish crud controllers for blog & user

--- server/src/controllers/blog.controller.ts
+++ server/src/controllers/blog.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Blog from "../models/blog.model";
+import mongoose, { isValidObjectId } from "mongoose";
 
 export async function get_blog(req: Request, res: Response) {
     const id = req.params.id
@@ -16,8 +17,14 @@ export async function get_blog(req: Request, res: Response) {
 
 export async function get_blogs(req: Request, res: Response) {
     const searchBlog = req.body
-    const blog = await Blog.find(searchBlog)
-    res.json(blog)
+    const blogs = await Blog.find(searchBlog)
+    if (!blogs) {
+        res.json({
+            message: "No such blogs"
+        })
+        return
+    }
+    res.status(200).json(blogs)
 }
 
 export async function create_blog(req: Request, res: Response) {
@@ -29,25 +36,48 @@ export async function create_blog(req: Request, res: Response) {
 
 export async function update_blog(req: Request, res: Response) {
     const id = req.params.id
-    const updatedBlog = Blog.updateOne({
-        _id: id
-    })
+    const updates = req.body
+    if (!id || !isValidObjectId(id)) {
+        res.status(400).json({
+            message: "Invalid document id"
+        })
+        return
+    }
+
+    let blog = await Blog.findById(id)
+    if (!blog) {
+        res.status(400).json({
+            message: "Invalid document id"
+        })
+        return
+    }
+
+    const updatedBlog = await Blog.updateOne({ _id: new mongoose.Types.ObjectId(id) }, updates)
+    if (!updatedBlog) {
+        res.json({
+            message: "Failed to update"
+        })
+        return
+    }
     res.json(updatedBlog)
 }
 
 export async function delete_blog(req: Request, res: Response) {
     const id = req.params.id
-    const deletedBlog = Blog.deleteOne({
-        _id: id
+    let blog = await Blog.findById(id)
+    if (!id || !blog) {
+        res.status(400).json({
+            message: "Invalid id provided or document doesn't exist"
+        })
+    }
+    let deletedBlog = await Blog.deleteOne({
+        _id: new mongoose.Types.ObjectId(id)
     })
+    if (!deletedBlog) {
+        res.json({
+            message: "Failed to update"
+        })
+        return
+    }
     res.json(deletedBlog)
-}
-
-// TODO
-export async function delete_blogs(req: Request, res: Response) {
-    const id = req.params.id
-    const deletedBlogs = Blog.deleteMany({
-        userId: id
-    })
-    res.json(deletedBlogs)
 }
--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,43 +1,108 @@
-import { json, Request, Response } from "express";
+import { Request, Response } from "express";
 import User from "../models/user.model";
 import Blog from "../models/blog.model";
+import mongoose, { isValidObjectId } from "mongoose";
+import * as EmailValidator from "email-validator"
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 export async function get_user(req: Request, res: Response) {
     const email = req.body.email
     const user = await User.findOne({
         email: email
     })
+    if (!user) {
+        res.status(404).json({
+            message: "Invalid user"
+        })
+        return
+    }
     res.json(user)
 }
 
 export async function create_user(req: Request, res: Response) {
-    const user = req.body
-    const createdUser = new User(user)
-    await createdUser.save()
+    let { email, password } = req.body
+    // check if email or password is missing
+    if (!email || !password) {
+        res.status(400).json({
+            message: "Email and password is required"
+        })
+        return
+    }
+
+    // email validation
+    if (!EmailValidator.validate(email)) {
+        res.status(400).json({
+            message: "Invalid email"
+        })
+        return
+    }
+
+    // encrypt password
+    const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+    const hashedPasswd = bcrypt.hashSync(password, salt);
+    if (!hashedPasswd) {
+        res.json({
+            message: "Failed to create user"
+        })
+        return
+    }
+
+    // create username from email
+    const username = email.split('@')[0];
+    let createdUser = new User({
+        username: username,
+        email: email,
+        password: hashedPasswd
+    })
+    createdUser = await createdUser.save()
     res.json(createdUser)
 }
 
 export async function update_user(req: Request, res: Response) {
-    const user = req.body
-    const userId = req.params.id
+    const updates = req.body
+    const id = req.params.id
+    if (!id || !isValidObjectId(id)) {
+        res.status(400).json({
+            message: "Invalid document id"
+        })
+        return
+    }
+
     const udpatedUser = await User.updateOne({
-        _id: userId
+        _id: new mongoose.Types.ObjectId(id)
     }, {
-        ...user
+        ...updates
     })
-    res.json(Object(udpatedUser))
+    if (!udpatedUser) {
+        res.json({
+            message: "Document doesn't exist"
+        })
+    }
+    res.json(udpatedUser)
 }
 
 export async function delete_user(req: Request, res: Response) {
     const userId = req.params.id
+    if (!userId || !isValidObjectId(userId)) {
+        res.status(400).json({
+            message: "Invalid document id"
+        })
+        return
+    }
     const deletedUser = await User.deleteOne({
         _id: userId
     })
+    if (!deletedUser) {
+        res.status(404).json({
+            message: "User doesn't exist"
+        })
+    }
     const deletedBlogs = await Blog.deleteMany({
         _id: userId
     })
     res.json({
-        "deletedUser": delete_user,
+        "deletedUser": deletedUser,
         "deletedBlogs": deletedBlogs
     })
 }
